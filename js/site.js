fe5f5632291568d6b5d614e76c877894aaf15f40
chrissywow <unknownchrissy@gmail.com>
2022-01-01 23:13:17 -0500

    cc22

--- js/site.js
+++ js/site.js
@@ -11,43 +11,67 @@ function getValues(){
     buzzValue = parseInt(buzzValue);
 
     if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)){
-        //we call generateNumbers
-        let numbers = generateNumbers(fizzValue,buzzValue);
+        //call fizzBuzz
+        let fbArray = fizzBuzz(fizzValue,buzzValue);
         //we call displayNumbers
-        displayNumbers(numbers);
+        displayData(fbArray);
     } else {
         alert("You must enter intergers");
     }
-
-    //we call fizzBuzz
-
-    //call displayData and write the values to the screen
-
 }
 
+// Do fizz Buzz
 function fizzBuzz(fizzValue, buzzValue)
 {
     let returnArray =[];
-
-    //loop from 1 to 100
-
-    //we need to check the current in three steps
+   //loop from 1 to 100
+    for (let i = 1; i <=100; i++){
     //check to see if divisible by Both ( 3 and 5)
-    //if so push 'Fizz' into an array and not the number
-
     //check to see if diviisble by fizz value ( 3)
-    //if so push 'Fizz' into an array and not the number
-
     //check to see if divisible by buzz value (5)
-    //if so push 'Buzz' into an array and not the number
+        if(i % fizzValue == 0 && i% buzzValue == 0){
+            returnArray.push('FizzBuzz');
+        }else if (i % fizzValue == 0){
+            returnArray.push('Fizz');
 
-    //if none then push the number into the array
-
-    returnArray;
+        }else if (i % buzzValue == 0){
+            returnArray.push('Buzz');
+        }else{
+            returnArray.push(i);
+        }
+    }
+    return returnArray;
 }
 
 function displayData(fbArray){
 //loop over the array and create a tablerow for each item.
+
+
+//get table body element from the page
+let tableBody = document.getElementById("results");
+
+//get the template row
+let templateRow = document.getElementById("fbTemplate");
+
+//clear the table first
+tableBody.innerHTML = "";
+
+for (let index = 0; index < fbArray.length; index += 5) {
+
+    let tableRow = document.importNode(templateRow.content, true);
+    
+    //grab use the to put into array
+    let rowCols = tableRow.querySelectorAll("td");
+    rowCols[0].textContent = fbArray[index];
+    rowCols[1].textContent = fbArray[index+1];
+    rowCols[2].textContent = fbArray[index+2];
+    rowCols[3].textContent = fbArray[index+3];
+    rowCols[4].textContent = fbArray[index+4];
+
+    tableBody.apeendChild(tableRow);
+
+}
+        
 
 //add all the rows to the table.
 }
