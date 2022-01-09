9c7ad665ddf9f8ed528e309c9fc163cf885a7233
chrissywow <unknownchrissy@gmail.com>
2022-01-08 19:52:30 -0500

    commit

--- js/site.js
+++ js/site.js
@@ -57,18 +57,26 @@ let templateRow = document.getElementById("fbTemplate");
 tableBody.innerHTML = "";
 
 for (let index = 0; index < fbArray.length; index += 5) {
-
     let tableRow = document.importNode(templateRow.content, true);
     
     //grab use the to put into array
     let rowCols = tableRow.querySelectorAll("td");
+    rowCols[0].classList.add(fbArray[index]);
     rowCols[0].textContent = fbArray[index];
+
+    rowCols[1].classList.add(fbArray[index +1]);
     rowCols[1].textContent = fbArray[index+1];
+
+    rowCols[2].classList.add(fbArray[index +2]);
     rowCols[2].textContent = fbArray[index+2];
+
+    rowCols[3].classList.add(fbArray[index +3]);
     rowCols[3].textContent = fbArray[index+3];
+
+    rowCols[4].classList.add(fbArray[index +4]);
     rowCols[4].textContent = fbArray[index+4];
 
-    tableBody.apeendChild(tableRow);
+    tableBody.appendChild(tableRow);
 
 }
         
